cc1491c0e3edf9bc92e5cab751d4c62970ed5746
Matthew Scharley <matt.scharley@gmail.com>
2016-05-21 18:44:05 +1000

    Don't define our own JSApp

--- src/main/scala/electron/ElectronApp.scala
+++ src/main/scala/electron/ElectronApp.scala
@@ -4,7 +4,8 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportDescendentObjects
 
 @JSExportDescendentObjects
-abstract class ElectronApp(rawElectron: raw.Electron) extends js.JSApp {
+abstract class ElectronApp(require: js.Function1[String, js.Any]) {
+  val rawElectron = require("electron").asInstanceOf[raw.Electron]
   implicit val electron = new Electron(rawElectron)
   lazy val process = js.Dynamic.global.process
 
